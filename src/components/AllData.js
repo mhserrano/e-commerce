c23483feddbd221f66509826a108ca829abb5fab
mhserrano <mariahcserrano@gmail.com>
2024-01-14 21:03:13 +0000

    Organized the products data

---
+++ src/components/AllData.js
@@ -0,0 +1,199 @@
+import Bed1 from "../img/products/beds/bed1.jpg";
+import Bed2 from "../img/products/beds/bed2.jpg";
+import Bed3 from "../img/products/beds/bed3.jpg";
+import Bed4 from "../img/products/beds/bed4.jpg";
+import Bed5 from "../img/products/beds/bed5.jpg";
+import Chair1 from "../img/products/chairs/chair1.jpg";
+import Chair2 from "../img/products/chairs/chair2.jpg";
+import Chair3 from "../img/products/chairs/chair3.jpg";
+import Chair4 from "../img/products/chairs/chair4.jpg";
+import Chair5 from "../img/products/chairs/chair5.jpg";
+import Lamp1 from "../img/products/lighting/lamp1.jpg";
+import Lamp2 from "../img/products/lighting/lamp2.jpg";
+import Lamp3 from "../img/products/lighting/lamp3.jpg";
+import Lamp4 from "../img/products/lighting/lamp4.jpg";
+import Lamp5 from "../img/products/lighting/lamp5.jpg";
+import Lamp6 from "../img/products/lighting/lamp6.jpg";
+import Plant1 from "../img/products/plants/plant1.jpg";
+import Plant2 from "../img/products/plants/plant2.jpg";
+import Plant3 from "../img/products/plants/plant3.jpg";
+import Plant4 from "../img/products/plants/plant4.jpg";
+import Plant5 from "../img/products/plants/plant5.jpg";
+import Sofa1 from "../img/products/sofas/sofa1.jpg";
+import Sofa2 from "../img/products/sofas/sofa2.jpg";
+import Sofa3 from "../img/products/sofas/sofa3.jpg";
+import Sofa4 from "../img/products/sofas/sofa4.jpg";
+import Sofa5 from "../img/products/sofas/sofa5.jpg";
+import Sofa6 from "../img/products/sofas/sofa6.jpg";
+import Sofa7 from "../img/products/sofas/sofa7.jpg";
+
+export const items = [
+  {
+    id: 1,
+    category: "bed",
+    img: Bed1,
+    price: 349,
+  },
+  {
+    id: 2,
+    category: "bed",
+    img: Bed2,
+    price: 299,
+  },
+  {
+    id: 3,
+    category: "bed",
+    img: Bed3,
+    price: 329,
+  },
+  {
+    id: 4,
+    category: "bed",
+    img: Bed4,
+    price: 490,
+  },
+  {
+    id: 5,
+    category: "bed",
+    img: Bed5,
+    price: 370,
+  },
+  {
+    id: 6,
+    category: "chair",
+    img: Chair1,
+    price: 270,
+  },
+  {
+    id: 7,
+    category: "chair",
+    img: Chair2,
+    price: 219,
+  },
+  {
+    id: 8,
+    category: "chair",
+    img: Chair3,
+    price: 130,
+  },
+  {
+    id: 9,
+    category: "chair",
+    img: Chair4,
+    price: 199,
+  },
+  {
+    id: 10,
+    category: "chair",
+    img: Chair5,
+    price: 189,
+  },
+  {
+    id: 11,
+    category: "lamp",
+    img: Lamp1,
+    price: 40,
+  },
+  {
+    id: 12,
+    category: "lamp",
+    img: Lamp2,
+    price: 49,
+  },
+  {
+    id: 13,
+    category: "lamp",
+    img: Lamp3,
+    price: 37,
+  },
+  {
+    id: 14,
+    category: "lamp",
+    img: Lamp4,
+    price: 70,
+  },
+  {
+    id: 15,
+    category: "lamp",
+    img: Lamp5,
+    price: 80,
+  },
+  {
+    id: 16,
+    category: "lamp",
+    img: Lamp6,
+    price: 110,
+  },
+  {
+    id: 17,
+    category: "plants",
+    img: Plant1,
+    price: 30,
+  },
+  {
+    id: 18,
+    category: "plants",
+    img: Plant2,
+    price: 39,
+  },
+  {
+    id: 19,
+    category: "plants",
+    img: Plant3,
+    price: 20,
+  },
+  {
+    id: 20,
+    category: "plants",
+    img: Plant4,
+    price: 15,
+  },
+  {
+    id: 21,
+    category: "plants",
+    img: Plant5,
+    price: 15,
+  },
+  {
+    id: 22,
+    category: "sofa",
+    img: Sofa1,
+    price: 220,
+  },
+  {
+    id: 23,
+    category: "sofa",
+    img: Sofa2,
+    price: 199,
+  },
+  {
+    id: 24,
+    category: "sofa",
+    img: Sofa3,
+    price: 280,
+  },
+  {
+    id: 25,
+    category: "sofa",
+    img: Sofa4,
+    price: 240,
+  },
+  {
+    id: 26,
+    category: "sofa",
+    img: Sofa5,
+    price: 300,
+  },
+  {
+    id: 27,
+    category: "sofa",
+    img: Sofa6,
+    price: 319,
+  },
+  {
+    id: 28,
+    category: "sofa",
+    img: Sofa7,
+    price: 289,
+  },
+];
