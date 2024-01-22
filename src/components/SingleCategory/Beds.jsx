1166f131005ef6cdb16b734785effff27c65d94e
mhserrano <mariahcserrano@gmail.com>
2024-01-22 00:15:36 +0000

    Filtered the items based on their category

--- src/components/SingleCategory/Beds.jsx
+++ src/components/SingleCategory/Beds.jsx
@@ -1,5 +1,26 @@
+import "../Categories.css";
+import { items } from "../AllData";
+
 function Beds() {
-  return <div className="container">Hello from Beds</div>;
+  const filteredItems = items.filter((item) => item.category === "bed");
+
+  return (
+    <div className="products-container">
+      <div className="products-grid">
+        {filteredItems.map((item) => {
+          return (
+            <div className="product" key={item.id}>
+              <img src={item.img} alt="product" />
+              <div className="product-details">
+                <p className="product-description">{item.description}</p>
+                <p className="price">{item.price}€</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Beds;
