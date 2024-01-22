e66f97c8475a913bc30f65bc4362a712af8d5502
mhserrano <mariahcserrano@gmail.com>
2024-01-22 00:48:50 +0000

    Filtered the rest of the items

--- src/components/SingleCategory/Chairs.jsx
+++ src/components/SingleCategory/Chairs.jsx
@@ -1,5 +1,25 @@
+import { items } from "../AllData";
+
 function Chairs() {
-  return <div className="container">Hello from Chairs</div>;
+  const filteredItems = items.filter((item) => item.category === "chair");
+
+  return (
+    <div className="products-container">
+      <div className="products-grid">
+        {filteredItems.map((item) => {
+          return (
+            <div className="product" key={item.id}>
+              <img src={item.img} alt="product" />
+              <div className="product-details">
+                <p className="product-description">{item.description}</p>
+                <p className="price">{item.price}€</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Chairs;
--- src/components/SingleCategory/Lighting.jsx
+++ src/components/SingleCategory/Lighting.jsx
@@ -1,5 +1,25 @@
+import { items } from "../AllData";
+
 function Lighting() {
-  return <div className="container">Hello from Lighting</div>;
+  const filteredItems = items.filter((item) => item.category === "lighting");
+
+  return (
+    <div className="products-container">
+      <div className="products-grid">
+        {filteredItems.map((item) => {
+          return (
+            <div className="product" key={item.id}>
+              <img src={item.img} alt="product" />
+              <div className="product-details">
+                <p className="product-description">{item.description}</p>
+                <p className="price">{item.price}€</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Lighting;
