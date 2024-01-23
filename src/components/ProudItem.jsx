97867c8e7b942d0acfb08f42cb89a494bf6d04af
mhserrano <mariahcserrano@gmail.com>
2024-01-23 22:25:28 +0000

    Styled the proud products section

--- src/components/ProudItem.jsx
+++ src/components/ProudItem.jsx
@@ -1,23 +1,27 @@
+import "../components/Categories.css";
+
 import { items } from "./AllData";
 
 function ProudItem() {
   let proudItems = items.filter((item) => item.proud);
 
   return (
-    <div className="products-grid">
+    <>
       <h2 className="proud-title">Products we are proud of</h2>
-      {proudItems.map((item) => {
-        return (
-          <div className="product" key={item.id}>
-            <img src={item.img} alt="product" />
-            <div className="product-details">
-              <p className="product-description">{item.description}</p>
-              <p className="price">{item.price}€</p>
+      <div className="products-grid">
+        {proudItems.map((item) => {
+          return (
+            <div className="product" key={item.id}>
+              <img src={item.img} alt="product" />
+              <div className="product-details">
+                <p className="product-description">{item.description}</p>
+                <p className="price">{item.price}€</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
