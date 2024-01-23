139bf7bc0e61bce30fda168a85b490b47ab4e25e
mhserrano <mariahcserrano@gmail.com>
2024-01-23 22:52:59 +0000

    Added the Banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import ProudProducts from "../components/ProudProducts";
+import Banner from "../components/Banner";
 
 function Home() {
   return (
     <>
       <Hero />
       <ProudProducts />
+      <Banner />
     </>
   );
 }
