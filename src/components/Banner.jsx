684c8eeb127fbb3b77f91d7d228b396d6966680f
mhserrano <mariahcserrano@gmail.com>
2024-01-24 17:20:29 +0000

    Added the link to the categories page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 function Banner() {
-  return null;
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <div className="banner-container">
+      <div className="text">
+        <h2>Creative harmonious living</h2>
+        <p>
+          Our products are all made to standard sizes so that you can mix and
+          match them freely.
+        </p>
+        <Link to="categories/all" onClick={handleClick}>
+          <button>shop now</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
