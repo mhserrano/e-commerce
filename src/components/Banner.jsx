56fc0754986c7fca7b36f33d29bd5055be472b96
mhserrano <mariahcserrano@gmail.com>
2024-01-24 18:56:59 +0000

    Styled the first banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,24 @@
-import { Link } from "react-router-dom";
+import "./Banner.css";
 
+import { Link } from "react-router-dom";
+import banner1 from "../img/banners/banner1.jpg";
 function Banner() {
-  const handleClick = () => {
-    window.scrollTo(0, 0);
-  };
   return (
     <div className="banner-container">
-      <div className="text">
-        <h2>Creative harmonious living</h2>
-        <p>
-          Our products are all made to standard sizes so that you can mix and
-          match them freely.
-        </p>
-        <Link to="categories/all" onClick={handleClick}>
-          <button>shop now</button>
-        </Link>
+      <div className="banner-flex">
+        <div className="text">
+          <h2>Creative harmonious living</h2>
+          <p>
+            Our products are all made to standard sizes so that you can mix and
+            match them freely.
+          </p>
+          <Link to="categories/all" onClick={() => window.scrollTo(0, 0)}>
+            <button>shop now</button>
+          </Link>
+        </div>
+        <div className="banner-img">
+          <img src={banner1} alt="banner" />
+        </div>
       </div>
     </div>
   );
