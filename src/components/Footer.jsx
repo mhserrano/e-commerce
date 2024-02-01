2c3d4cb3eba8145ec7ee54bd3ed5f615aeda357c
mhserrano <mariahcserrano@gmail.com>
2024-02-01 00:19:22 +0000

    Styled the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,33 @@
+import "./Footer.css";
+
 function Footer() {
-  return <>hello from footer</>;
+  return (
+    <footer>
+      <div className="footer-links">
+        <a href="/">About</a>
+
+        <a href="/">Store locator</a>
+
+        <a href="/">FAQs</a>
+
+        <a href="/">News</a>
+
+        <a href="/">Careers</a>
+
+        <a href="/">Contact Us</a>
+      </div>
+      <div className="by">
+        <p>
+          This project was coded by
+          <a href="https://github.com/mhserrano"> Maria Serrano</a> and is
+          <a href="https://github.com/mhserrano/e-commerce"> open-sourced.</a>
+          <br />
+          Design inspired by
+          <a href="https://github.com/Abderraouf-Rahmani"> Abderraouf</a>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
