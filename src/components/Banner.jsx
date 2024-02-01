59b8ca00329174257a6430fe8703418c78d935c6
mhserrano <mariahcserrano@gmail.com>
2024-02-01 23:25:58 +0000

    optimized the code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,20 @@
 import "./Banner.css";
 
 import { Link } from "react-router-dom";
-import banner1 from "../img/banners/banner1.jpg";
-function Banner() {
+
+function Banner({ title, text, img }) {
   return (
     <div className="banner-container">
       <div className="banner-flex">
         <div className="text">
-          <h2>Creative harmonious living</h2>
-          <p>
-            Our products are all made to standard sizes so that you can mix and
-            match them freely.
-          </p>
+          <h2>{title}</h2>
+          <p>{text}</p>
           <Link to="categories/all" onClick={() => window.scrollTo(0, 0)}>
             <button>shop now</button>
           </Link>
         </div>
         <div className="banner-img">
-          <img src={banner1} alt="banner" />
+          <img src={img} alt="banner" />
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,24 @@ import TrendingSlider from "../components/TrendingSlider";
 import BannerReverse from "../components/BannerReverse";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
-
+import banner1 from "../img/banners/banner1.jpg";
+import banner2 from "../img/banners/banner2.jpg";
 function Home() {
   return (
     <>
       <Hero />
       <ProudProducts />
-      <Banner />
+      <Banner
+        title="Creative harmonious living"
+        text=" Our Products are all made to standard sizes so that you can mix and match them freely."
+        img={banner1}
+      />
       <TrendingSlider />
-      <BannerReverse />
+      <BannerReverse
+        title="Comfortable & Elegante Living"
+        text=" Our Products are all made to standard sizes so that you can mix and match them freely."
+        img={banner2}
+      />
       <Newsletter />
       <Footer />
     </>
