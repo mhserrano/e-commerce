c553ee7e37f8526f57fc36eebdc3ea504efa8a73
mhserrano <mariahcserrano@gmail.com>
2024-02-02 23:25:26 +0000

    Imported small images to AllData.js

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,31 +1,103 @@
 import Bed1 from "../img/products/beds/bed1/bed1.jpg";
+import smallBed1A from "../img/products/beds/bed1/smallBed1A.jpg";
+
 import Bed2 from "../img/products/beds/bed2/bed2.jpg";
+import smallBed2A from "../img/products/beds/bed2/smallBed2A.jpg";
+import smallBed2B from "../img/products/beds/bed2/smallBed2B.jpg";
+
 import Bed3 from "../img/products/beds/bed3/bed3.jpg";
+import smallBed3A from "../img/products/beds/bed3/smallBed3A.jpg";
+
 import Bed4 from "../img/products/beds/bed4/bed4.jpg";
+import smallBed4A from "../img/products/beds/bed4smallBed4A.jpg";
+
 import Bed5 from "../img/products/beds/bed5/bed5.jpg";
+import smallBed5A from "../img/products/beds/bed5/smallBed5A.jpg";
+import smallBed5B from "../img/products/beds/bed5/smallBed5B.jpg";
+
 import Chair1 from "../img/products/chairs/chair1/chair1.jpg";
+import smallChair1A from "../img/products/chairs/chair1/smallChair1A.jpg";
+import smallChair1B from "../img/products/chairs/chair1/smallChair1B.jpg";
+
 import Chair2 from "../img/products/chairs/chair2/chair2.jpg";
+import smallChair2A from "../img/products/chairs/chair2/smallChair2A.jpg";
+import smallChair2B from "../img/products/chairs/chair2/smallChair2B.jpg";
+
 import Chair3 from "../img/products/chairs/chair3/chair3.jpg";
+import smallChair3A from "../img/products/chairs/chair3/smallChair3A.jpg";
+import smallChair3B from "../img/products/chairs/chair3/smallChair3B.jpg";
+
 import Chair4 from "../img/products/chairs/chair4/chair4.jpg";
+import smallChair4A from "../img/products/chairs/chair4/smallChair4A.jpg";
+import smallChair4B from "../img/products/chairs/chair4/smallChair4B.jpg";
+
 import Chair5 from "../img/products/chairs/chair5/chair5.jpg";
+import smallChair5A from "../img/products/chairs/chair5/smallChair5A.jpg";
+import smallChair5B from "../img/products/chairs/chair5/smallChair5B.jpg";
+
 import Lamp1 from "../img/products/lighting/lamp1/lamp1.jpg";
+import smallLamp1A from "../img/products/lighting/lamp1/smallLamp1A.jpg";
+import smallLamp1B from "../img/products/lighting/lamp1/smallLamp1B.jpg";
+
 import Lamp2 from "../img/products/lighting/lamp2/lamp2.jpg";
+import smallLamp2A from "../img/products/lighting/lamp2/smallLamp2A.jpg";
+import smallLamp2B from "../img/products/lighting/lamp2/smallLamp2B.jpg";
+
 import Lamp3 from "../img/products/lighting/lamp3/lamp3.jpg";
+import smallLamp3A from "../img/products/lighting/lamp3/smallLamp3A.jpg";
+import smallLamp3B from "../img/products/lighting/lamp3/smallLamp3B.jpg";
+
 import Lamp4 from "../img/products/lighting/lamp4/lamp4.jpg";
+import smallLamp4A from "../img/products/lighting/lamp4/smallLamp4A.jpg";
+
 import Lamp5 from "../img/products/lighting/lamp5/lamp5.jpg";
+import smallLamp5A from "../img/products/lighting/lamp5/smallLamp5A.jpg";
+import smallLamp5B from "../img/products/lighting/lamp5/smallLamp5B.jpg";
+
 import Lamp6 from "../img/products/lighting/lamp6/lamp6.jpg";
+import smallLamp6A from "../img/products/lighting/lamp6/smallLamp6A.jpg";
+import smallLamp6B from "../img/products/lighting/lamp6/smallLamp6B.jpg";
+
 import Plant1 from "../img/products/plants/plant1/plant1.jpg";
+import smallPlant1A from "../img/products/plants/plant1/smallPlant1A.jpg";
+import smallPlant1B from "../img/products/plants/plant1/smallPlant1B.jpg";
+
 import Plant2 from "../img/products/plants/plant2/plant2.jpg";
+import smallPlant2A from "../img/products/plants/plant1/smallPlant2A.jpg";
+import smallPlant2B from "../img/products/plants/plant1/smallPlant2B.jpg";
+
 import Plant3 from "../img/products/plants/plant3/plant3.jpg";
+import smallPlant3A from "../img/products/plants/plant1/smallPlant3A.jpg";
+
 import Plant4 from "../img/products/plants/plant4/plant4.jpg";
+import smallPlant4A from "../img/products/plants/plant1/smallPlant4A.jpg";
+import smallPlant4B from "../img/products/plants/plant1/smallPlant4B.jpg";
+
 import Plant5 from "../img/products/plants/plant5/plant5.jpg";
+import smallPlant5A from "../img/products/plants/plant1/smallPlant5A.jpg";
+
 import Sofa1 from "../img/products/sofas/sofa1/sofa1.jpg";
+import smallSofa1A from "../img/products/sofas/sofa1/smallSofa1A.jpg";
+
 import Sofa2 from "../img/products/sofas/sofa2/sofa2.jpg";
+import smallSofa2A from "../img/products/sofas/sofa2/smallSofa2A.jpg";
+
 import Sofa3 from "../img/products/sofas/sofa3/sofa3.jpg";
+import smallSofa3A from "../img/products/sofas/sofa3/smallSofa3A.jpg";
+import smallSofa3B from "../img/products/sofas/sofa3/smallSofa3B.jpg";
+
 import Sofa4 from "../img/products/sofas/sofa4/sofa4.jpg";
+import smallSofa4A from "../img/products/sofas/sofa4/smallSofa4A.jpg";
+
 import Sofa5 from "../img/products/sofas/sofa5/sofa5.jpg";
+import smallSofa5A from "../img/products/sofas/sofa5/smallSofa5A.jpg";
+import smallSofa5B from "../img/products/sofas/sofa5/smallSofa5B.jpg";
+
 import Sofa6 from "../img/products/sofas/sofa6/sofa6.jpg";
+import smallSofa6A from "../img/products/sofas/sofa6/smallSofa6A.jpg";
+
 import Sofa7 from "../img/products/sofas/sofa7/sofa7.jpg";
+import smallSofa7A from "../img/products/sofas/sofa7/smallSofa7A.jpg";
 
 export const items = [
   {
