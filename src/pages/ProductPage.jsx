d1c5acbb7bd8c4e5d236fce530bc55451fbe0a37
mhserrano <mariahcserrano@gmail.com>
2024-02-04 21:46:06 +0000

    Added style to the quantity buttons

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -10,20 +10,20 @@ function ProductPage() {
           <h3 className="product-title">Sophisticated Plush Suede Bed</h3>
           <div className="product-info">
             <div className="img-side">
-              <img src={items[16].img} alt="product" width={700} />
+              <img src={items[6].img} alt="product" width={700} />
             </div>
             <div className="details-side">
               <div className="small-imgs">
                 <img src={items[16].img} width={170} />
-                <img src={items[16].otherImgs[0]} width={170} />
+                <img src={items[19].otherImgs[0]} width={170} />
                 <img src={items[16].otherImgs[1]} width={170} />
               </div>
-              <div className="quantity">
+              <div className="quantity-price">
                 <div className="quantity-row">
                   <p>quantity</p>
                   <div className="product-btns">
                     <button>-</button>
-                    <div className="quantity">1</div>
+                    <div className="quantity-counter">1</div>
                     <button>+</button>
                   </div>
                   <p className="product-price">price €</p>
